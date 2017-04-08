2c75a0ed745d0bdcf6f1c2c86d49f52923b08ba9
roxroy <roxroy@devicode.com>
2017-04-08 15:01:52 +0000

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Request Header Parser Microservice
+# Request Header Parser Microservice
 
 Here are the specific user story you should implement for this project:  
-User Story:  I can get the IP address, language and operating system for my browser.
+User Story: I can get the IP address, language and operating system for my browser.
--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-var useragent = require('express-useragent');
 var express = require('express')
-
+var path = require('path')
+var whoami = require('./whoami');
 var app = express()
 
-function getIp (headers, client){
-     return ( headers["X-Forwarded-For"]
-                || headers["x-forwarded-for"]
-                || client.remoteAddress );
-}
+app.use(express.static(path.join(__dirname, '')))
+app.set('view engine', 'pug')
+app.set('views','./')
 
-function getLang (langs){
-    // Return the first one
-    return  langs ? langs[0] : "unknown";
-}
-
-function getSoftware (uaSource){
-    var ua = useragent.parse(uaSource);
-    var source = ua.source;
-    
-    /*
-       Get the first section in parentheses
-       
-        \( : match an opening parentheses
-        ( : begin capturing group
-        [^)]+: match one or more non ) characters
-        ) : end capturing group
-        \) : match closing parentheses
-    */
-
-    var regExp = /\(([^)]+)\)/;
-    var matches = regExp.exec(source);
-    
-    return matches[1];
-}
-
-app.get('/api/whoami', function(req, res) {
+app.get('/whoami', function(req, res) {
     var result = { ipaddress: undefined , language : undefined , software :undefined};
     
-    result.ipaddress =  getIp(req.headers, req.client);
-    result.language =  getLang(req.acceptsLanguages());
-    result.software =  getSoftware(req.headers['user-agent']);
+    result.ipaddress =  whoami.getIp(req.headers, req.client);
+    result.language =  whoami.getLang(req.acceptsLanguages());
+    result.software =  whoami.getSoftware(req.headers['user-agent']);
     
     res.json(result)
+})
+
+app.get('/', function(req, res) {
+ var now = new Date()
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+    res.render('index', { url : fullUrl}  )
 })
 
 app.listen(process.env.PORT || 3000)
